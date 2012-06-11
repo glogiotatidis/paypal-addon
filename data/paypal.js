c7734fe0ace10a4efa6f4933600ecf0ba51aad67
Giorgos Logiotatidis <seadog@sealabs.net>
2012-06-11 15:35:07 +0300

    Beautify and enhance UX.

--- data/paypal.js
+++ data/paypal.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
     if (document.title == 'Send Money - PayPal') {
-	$('<a id="paypal-addon-recalculate">Add Transaction Costs</a>').insertAfter($('#amount'));
+	console.log(window.s);
+	$('<a id="paypal-addon-recalculate" style="padding-left: 4px; color: #ccc;">Add Transaction Costs</a>').insertAfter($('#amount'));
 
-	$('#paypal-addon-recalculate').click(function(event) {
-	    event.preventDefault();
-	    amount = $('#amount').val();
-	    amount = amount * 1.04;
-	    $('#amount').val(amount);
+	$('#amount').bind('propertychange keyup input paste', function(event_change) {
+	    if ($('#paypal-addon-recalculate').hasClass('paypal-addon-ready'))
+		return;
+
+	    $('#paypal-addon-recalculate').css('color', 'green');
+	    $('#paypal-addon-recalculate').css('cursor', 'pointer');
+	    $('#paypal-addon-recalculate').addClass("paypal-addon-ready");
+
+	    $('#paypal-addon-recalculate').one('click', function(event) {
+		event.preventDefault();
+		amount = $('#amount').val();
+		amount = amount * 1.04;
+		$('#amount').val(amount);
+		$('#paypal-addon-recalculate').unbind('click');
+		$('#paypal-addon-recalculate').css('color', '#ccc');
+		$('#paypal-addon-recalculate').css('cursor', 'default');
+		$('#paypal-addon-recalculate').removeClass("paypal-addon-ready");
+	    });
 	});
+
     }
 });
